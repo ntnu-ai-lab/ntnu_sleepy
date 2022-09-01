2b3c0388a3b0f0d20786abd5e51d71c06625c50d
Karl Erik Hofseth <karl.e.hofseth@infomedia.dk>
2022-09-01 11:57:20 +0200

    Rename a folder

--- api/somnus/urls.py
+++ api/somnus/urls.py
@@ -1,4 +1,4 @@
-"""api URL Configuration
+"""somnus URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('somnus-auth/', include('rest_framework.urls'))
 ]
