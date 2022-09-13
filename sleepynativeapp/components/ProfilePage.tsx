d9013872a8cd4f5fe43295440df51e0aaf740a15
hildmi@stud.ntnu.no <hildmi@stud.ntnu.no>
2022-09-13 13:40:04 +0200

    params change

--- sleepynativeapp/components/ProfilePage.tsx
+++ sleepynativeapp/components/ProfilePage.tsx
@@ -11,47 +11,47 @@ export function ProfilePage() {
 
     const [state, setState] = useState(UserEx);
 
-    const onEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => 
+    const onEmailChange = (arg: string) => 
     setState((prev) => ({
         ...prev,
-        email : e.target.value,
+        email : arg,
     }));
 
-    const onPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => 
+    const onPasswordChange = (arg: string) => 
     setState((prev) => ({
         ...prev,
-        password : e.target.value,
+        password : arg,
     }));
 
-    const onGenderChange = (e: React.ChangeEvent<HTMLSelectElement>) => 
+    const onGenderChange = (arg: string) => 
     setState((prev) => ({
         ...prev,
-        gender : (e.target.value as unknown as gender),
+        gender : (arg as gender),
     }));
 
    
-    const onOccupationChange = (e: React.ChangeEvent<HTMLInputElement>) => 
+    const onOccupationChange = (arg: string) => 
     setState((prev) => ({
         ...prev,
-        occupation : e.target.value,
+        occupation : arg,
     }));
 
-    const onNameChange = (e: React.ChangeEvent<HTMLInputElement>) => 
+    const onNameChange = (arg: string) => 
     setState ((prev) => ({
         ...prev,
-        name: e.target.value,
+        name: arg,
     }));
 
-    const onDateOfBirthChange = (e: React.ChangeEvent<HTMLInputElement>) => 
+    const onDateOfBirthChange = (arg: string) => 
     setState ((prev) => ({
         ...prev,
-        dateOfBirth : e.target.value,
+        dateOfBirth : arg,
     }));
 
-    const onRelationshipStatusChange = (e: React.ChangeEvent<HTMLSelectElement>) => 
+    const onRelationshipStatusChange = (arg: string) => 
     setState ((prev) => ({
         ...prev,
-        relationshipStatus : (e.target.value as unknown as relationshipStatus),
+        relationshipStatus : (arg as relationshipStatus),
     }));
 
     return (
@@ -60,23 +60,23 @@ export function ProfilePage() {
                 <Text style={{paddingBottom: "10%" ,fontSize: 25, alignSelf: "center"}}>Din profil</Text>
                 <Card style={{padding: 20}}>
                     <Text style={{color: "white"}}>Email</Text>
-                    <TextField value={state.email} onChange={() => {onEmailChange}} placeholderText={state.email}/>
+                    <TextField value={state.email} onChange={onEmailChange} placeholderText={state.email}/>
                     <Text style={{color: "white"}}>Navn</Text>
-                    <TextField value={state.name} onChange={() => {onNameChange}} placeholderText={state.name}/>
+                    <TextField value={state.name} onChange={onNameChange} placeholderText={state.name}/>
                     <Text style={{color: "white"}}>Passord</Text>
-                    <TextField value={state.password} onChange={() => {onPasswordChange}} placeholderText={state.password}/> 
+                    <TextField value={state.password} onChange={onPasswordChange} placeholderText={state.password}/> 
                     <Text style={{color: "white"}}>Fødselsdato</Text>
-                    <TextField value={state.dateOfBirth} onChange={() => {onDateOfBirthChange}} placeholderText={state.dateOfBirth}/> 
+                    <TextField value={state.dateOfBirth} onChange={onDateOfBirthChange} placeholderText={state.dateOfBirth}/> 
                     <Text style={{color: "white"}}>Kjønn</Text>
-                    <Select value={state.gender} onChange={() => { onGenderChange; } } placeholderText={state.gender} options={["male", "female", "other"]} optionDisplay={(arg: gender) => {
+                    <Select value={state.gender} onChange={onGenderChange} placeholderText={state.gender} options={["male", "female", "other"]} optionDisplay={(arg: gender) => {
                     if (arg === "male") return "Mann"
                     else if (arg === "female") return "Kvinne"
                     else return "Annet"
                     }}/>
                     <Text style={{color: "white"}}>Yrke</Text>
-                    <TextField value={state.occupation} onChange={() => {onOccupationChange}} placeholderText={state.occupation}/>
+                    <TextField value={state.occupation} onChange={onOccupationChange} placeholderText={state.occupation}/>
                     <Text style={{color: "white"}}>Sivilstatus</Text>
-                    <Select value={state.relationshipStatus} onChange={() => { onRelationshipStatusChange; } } placeholderText={state.relationshipStatus} options={["married", "coliving", "relationship", "single"]} optionDisplay={ (arg: relationshipStatus) => {
+                    <Select value={state.relationshipStatus} onChange={onRelationshipStatusChange} placeholderText={state.relationshipStatus} options={["married", "coliving", "relationship", "single"]} optionDisplay={ (arg: relationshipStatus) => {
                     if (arg === "married") return "Gift"
                     if (arg === "coliving") return "Samboer"
                     if (arg === "relationship") return "Fast forhold"
