3e1759dae765b18cfc1fef983c9e6a5624185869
Karl Erik Hofseth <karl@kehofseth.no>
2022-09-13 12:19:28 +0200

    Add urls

--- api/somnus/modules/urls.py
+++ api/somnus/modules/urls.py
@@ -1,7 +1,14 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 
+from .views import ModuleViewSet, PageViewSet, SectionViewSet
+
 router = DefaultRouter()
+
+
+router.register('pages', PageViewSet)
+router.register('sections', SectionViewSet)
+router.register('modules', ModuleViewSet)
 
 
 urlpatterns = [
