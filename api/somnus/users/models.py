f7dd2fa39d2a0483bb628884599b0d56e687475b
Karl Erik Hofseth <karl@kehofseth.no>
2022-09-13 14:17:36 +0200

    Update user model

--- api/somnus/users/models.py
+++ api/somnus/users/models.py
@@ -4,7 +4,5 @@ from django.db import models
 from django.contrib.auth import models as auth_models
 
 
-class User(auth_models.AbstractBaseUser):
+class User(auth_models.AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
