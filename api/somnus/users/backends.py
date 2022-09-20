e8a9e60200069628a7e05ff5c4b4e3e095a13b4c
Magnus Lykke Dahlø <magnusld@stud.ntnu.no>
2022-09-20 13:36:10 +0200

    Fix mypy

--- api/somnus/users/backends.py
+++ api/somnus/users/backends.py
@@ -25,7 +25,7 @@ class OryBackend(BaseBackend):
         user_id = sess.json().get('identity', {}).get('id', None)
         if not user_id:
             return None
-        user, created = User._default_manager.get_or_create( # type: ignore[misc]
+        user, created = User._default_manager.get_or_create(
             **{User.USERNAME_FIELD: user_id, 'id': user_id}
         )
 
