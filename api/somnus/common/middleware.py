e483f01d04a8abaed0bcb82ed1e42902dc194f37
Karl Erik Hofseth <karl@kehofseth.no>
2022-09-20 12:42:59 +0200

    Fix middleware factory

--- api/somnus/common/middleware.py
+++ api/somnus/common/middleware.py
@@ -1,7 +1,9 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-def mobile_csrf_middleware(request: Request) -> Response:
-    if (request.headers.get('X-Session-Token')):
-        setattr(request, '_dont_enforce_csrf_checks', True)
-    return request
+def mobile_csrf_middleware(get_response):
+    def middleware(request: Request) -> Response:
+        if (request.headers.get('X-Session-Token')):
+            setattr(request, '_dont_enforce_csrf_checks', True)
+        return get_response(request)
+    return middleware
