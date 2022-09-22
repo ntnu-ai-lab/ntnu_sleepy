e7142b2fa13c2bd0ea5b285a9973021fb9abbd7c
Magnus Lykke Dahlø <magnusld@stud.ntnu.no>
2022-09-22 11:26:22 +0200

    Recoil +++ (#44)
    * #10 oppdater signup flow
    
    * legge inn navbar på sider, legge til StorageController, lagre bruker i
    
    * #10 refactor page template
    
    * fjerne debug console.logs

--- sleepynativeapp/components/GeneralPage.tsx
+++ sleepynativeapp/components/GeneralPage.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { Card } from "./material/Card";
-import { View, Text} from "react-native";
+import { View, Text, StyleSheet} from "react-native";
 import { PageTemplate } from "./PageTemplate";
 import { useNavigation } from "@react-navigation/native";
+import { NavBar } from "./material/NavBar";
+import { colors } from "../styles/styles";
 
 //page for general tips for sleeping. Should be available to all users when logged in.
 export function GeneralPage() {
     const navigation = useNavigation();
 
+    const styles = StyleSheet.create({
+        text: {
+            color: colors.text_white
+        }
+    })
+
     return(
         <PageTemplate>
             <View>
                 <Card style={{padding:50}}>
-                    <Text>Sov i gjennomsnitt 8 timer hver dag</Text>
-                    <Text>Ikke bruk skjerm på mobil,tablet eller laptop 2 timer før leggetid</Text>
-                    <Text>Bruk sengen kun til soving</Text>
+                    <Text style={styles.text}>Sov i gjennomsnitt 8 timer hver dag</Text>
+                    <Text style={styles.text}>Ikke bruk skjerm på mobil,tablet eller laptop 2 timer før leggetid</Text>
+                    <Text style={styles.text}>Bruk sengen kun til soving</Text>
                 </Card>
             </View>
+            <NavBar />
         </PageTemplate>
     )
 }
--- sleepynativeapp/components/Navigation.tsx
+++ sleepynativeapp/components/Navigation.tsx
@@ -1,31 +1,42 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { AuthContext } from "../auth/AuthProvider";
+import { GeneralPage } from "./GeneralPage";
+import { HomePage } from "./HomePage";
 import { LoginPage } from "./LoginPage";
 import { ProfilePage } from "./ProfilePage";
 import { SignupPage } from "./SignUpPage";
 
 export function Navigation() {
-    const Stack = createNativeStackNavigator();
-    const { isAuthenticated } = useContext(AuthContext)
+  const Stack = createNativeStackNavigator();
+  const { isAuthenticated } = useContext(AuthContext);
 
-    return (
-        <NavigationContainer>
-          <Stack.Navigator
-            screenOptions={{
-              headerShown: false,
-              headerTransparent: true,
-              statusBarTranslucent: true,
-              statusBarColor: "transparent",
-              animation: "none",
-            }}
-            initialRouteName="login"
-          >
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          headerTransparent: true,
+          statusBarTranslucent: true,
+          statusBarColor: "transparent",
+          animation: "none",
+        }}
+        initialRouteName="login"
+      >
+        {isAuthenticated ? (
+          <>
+            <Stack.Screen name="profile" component={ProfilePage} />
+            <Stack.Screen name="home" component={HomePage} />
+            <Stack.Screen name="assignment" component={GeneralPage} />
+          </>
+        ) : (
+          <>
             <Stack.Screen name="login" component={LoginPage} />
             <Stack.Screen name="signup" component={SignupPage} />
-            <Stack.Screen name="profile" component={ProfilePage} />
-          </Stack.Navigator>
-        </NavigationContainer>
-    )
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
