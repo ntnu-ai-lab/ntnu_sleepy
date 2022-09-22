b8d3177ccf8bf749c6950cad2051aec1301e9a79
Magnus Lykke Dahlø <magnusld@stud.ntnu.no>
2022-09-22 15:21:11 +0200

    feat(#47): Legge til text, og starten av image component

--- sleepynativeapp/components/module/ImageSectionComponent.tsx
+++ sleepynativeapp/components/module/ImageSectionComponent.tsx
@@ -1,8 +1,25 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { colors } from "../../styles/styles";
+import { ImageSection } from "../../types/modules";
 import { Card } from "../material/Card";
 
-export function ImageSectionComponent() {
+export function ImageSectionComponent(props: {
+    section: ImageSection
+}) {
+
+    const {section} = props
+
+    const styles = StyleSheet.create({
+        heading: {
+            color: colors.text_white,
+            fontSize: 20 
+        },
+        img: {
+
+        }
+    })
+
     return (
         <Card>
             
