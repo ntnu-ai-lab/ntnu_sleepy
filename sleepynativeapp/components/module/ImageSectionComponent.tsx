989ed22f8e84877c9182608524c556a183257174
Magnus Lykke Dahlø <magnusld@stud.ntnu.no>
2022-09-22 15:36:19 +0200

    merge

--- sleepynativeapp/components/module/ImageSectionComponent.tsx
+++ sleepynativeapp/components/module/ImageSectionComponent.tsx
@@ -1,28 +1,29 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image, Text } from "react-native";
 import { colors } from "../../styles/styles";
 import { ImageSection } from "../../types/modules";
 import { Card } from "../material/Card";
 
-export function ImageSectionComponent(props: {
-    section: ImageSection
-}) {
+export function ImageSectionComponent(props: { section: ImageSection }) {
+  const { section } = props;
 
-    const {section} = props
+  const styles = StyleSheet.create({
+    heading: {
+      color: colors.text_white,
+      fontSize: 20,
+    },
+    img: {},
+    caption: {
+      color: colors.text_secondary,
+      fontSize: 12,
+    },
+  });
 
-    const styles = StyleSheet.create({
-        heading: {
-            color: colors.text_white,
-            fontSize: 20 
-        },
-        img: {
-
-        }
-    })
-
-    return (
-        <Card>
-            
-        </Card>
-    )
+  return (
+    <Card>
+      <Text style={styles.heading}></Text>
+      <Image style={styles.img} source={{ uri: section.uri }} />
+      <Text style={styles.caption}></Text>
+    </Card>
+  );
 }
