41d8df1059f167399578402bec4d3caf2c018d98
simonevo <simonevo@stud.ntnu.no>
2022-09-27 13:54:44 +0200

    Add old pages

---
+++ sleepynativeapp/components/OldLoginPage.tsx
@@ -0,0 +1,140 @@
+import { SelfServiceLoginFlow, SubmitSelfServiceLoginFlowBody, SubmitSelfServiceLoginFlowWithPasswordMethodBody } from "@ory/kratos-client";
+import { useFocusEffect, useNavigation, useRoute } from "@react-navigation/native";
+import React, { useContext, useEffect, useState } from "react";
+import { View, Text, Dimensions } from "react-native";
+import { SessionContext } from "../auth/Auth";
+import { AuthContext } from "../auth/AuthProvider";
+import { handleFormSubmitError } from "../auth/form";
+import { ProjectContext } from "../auth/ProjectProvider";
+import { newKratosSdk } from "../auth/Sdk";
+import { Button } from "./material/Button";
+import { Card } from "./material/Card";
+import { TextField } from "./material/TextField";
+import { PageTemplate } from "./PageTemplate";
+
+export function OldLoginPage() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const navigation = useNavigation();
+  const { project } = useContext(ProjectContext);
+  const { setSession, session, sessionToken, isAuthenticated } = useContext(AuthContext);
+  const [flow, setFlow] = useState<SelfServiceLoginFlow | undefined>(undefined);
+  const route = useRoute();
+
+  const initializeFlow = () =>
+    newKratosSdk(project)
+      .initializeSelfServiceLoginFlowWithoutBrowser(
+        true,
+        undefined,
+        sessionToken
+      )
+      .then((response) => {
+        const { data: flow } = response;
+        // The flow was initialized successfully, let's set the form data:
+        setFlow(flow);
+      })
+      .catch(console.error);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      initializeFlow();
+
+      return () => {
+        setFlow(undefined);
+      };
+    }, [project])
+  );
+
+  useEffect(() => {
+    if (isAuthenticated && sessionToken && session) {
+      //@ts-ignore
+      navigation.navigate("profile")
+    }
+  }, [isAuthenticated])
+
+  if (isAuthenticated) {
+    return null
+  }
+
+  const onSubmit = (payload: SubmitSelfServiceLoginFlowBody) =>
+    flow
+      ? newKratosSdk(project)
+          .submitSelfServiceLoginFlow(flow.id, payload, sessionToken)
+          .then(({ data }) => Promise.resolve(data as SessionContext))
+          // Looks like everything worked and we have a session!
+          .then((session) => {
+            setSession(session)
+            setTimeout(() => { // @ts-ignore
+              navigation.navigate("profile")
+            }, 100)
+          })
+          .catch(handleFormSubmitError(setFlow, initializeFlow))
+      : Promise.resolve()
+
+  return (
+    <PageTemplate>
+      <View
+        style={{
+          height: Dimensions.get("window").height,
+          width: "100%",
+          paddingHorizontal: 20,
+          justifyContent: "center",
+        }}
+      >
+        <Card style={{ padding: 20 }}>
+          <TextField
+            value={email}
+            onChange={setEmail}
+            placeholderText={"Epost"}
+          />
+          <TextField
+            value={password}
+            onChange={setPassword}
+            password
+            placeholderText={"Passord"}
+          />
+          <Button variant="contained" onClick={() => {
+            const userInput: SubmitSelfServiceLoginFlowWithPasswordMethodBody = {
+              identifier: email,
+              method: "password",
+              password: password,
+            }
+            onSubmit(userInput)
+          }}>
+            <Text style={{ fontSize: 20 }}>Log in</Text>
+          </Button>
+        </Card>
+        <Button
+          onClick={() => {
+            //@ts-ignore'
+            navigation.navigate("signup");
+          }}
+        >
+          <Text style={{ fontSize: 20 }}>Opprett ny bruker</Text>
+        </Button>
+        <View>
+          <Button style={{ height: 20 }}>
+            <Text style={{ fontSize: 14 }}>Glemt passord?</Text>
+          </Button>
+          <Button
+            style={{ height: 20 }}
+            onClick={() =>
+              { //@ts-ignore
+                navigation.navigate("profile");
+              }
+            }
+          >
+            <Text style={{ fontSize: 14 }}>Midlertidig profilside test</Text>
+          </Button>
+        </View>
+        <View>
+          <Button style={{height: 20}}
+          onClick={() => { //@ts-ignore
+            navigation.navigate("tools");
+          }}><Text style={{fontSize: 14}}>Midlertidig modulside </Text>
+            </Button>
+        </View>
+      </View>
+    </PageTemplate>
+  );
+}
---
+++ sleepynativeapp/components/OldNavigation.tsx
@@ -0,0 +1,45 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React, { useContext, useEffect } from "react";
+import { AuthContext } from "../auth/AuthProvider";
+import { GeneralPage } from "./GeneralPage";
+import { HomePage } from "./HomePage";
+import { LoginPage } from "./LoginPage";
+import { ProfilePage } from "./ProfilePage";
+import { SignupPage } from "./SignUpPage";
+import { TestModulePage } from "./TestModulePage";
+
+export function OldNavigation() {
+  const Stack = createNativeStackNavigator();
+  const { isAuthenticated } = useContext(AuthContext);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          headerTransparent: true,
+          statusBarTranslucent: true,
+          statusBarColor: "transparent",
+          animation: "none",
+        }}
+        initialRouteName="login"
+      >
+        {isAuthenticated ? (
+          <>
+            <Stack.Screen name="profile" component={ProfilePage} />
+            <Stack.Screen name="home" component={HomePage} />
+            <Stack.Screen name="tools" component={TestModulePage} />
+            <Stack.Screen name="assignment" component={GeneralPage} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="login" component={LoginPage} />
+            <Stack.Screen name="tools" component={TestModulePage} />
+            <Stack.Screen name="signup" component={SignupPage} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
