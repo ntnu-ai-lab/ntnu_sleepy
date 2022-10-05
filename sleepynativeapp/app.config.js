c2a56ba506db2e16f55d85170977f3b91ad53cfe
Karl Erik Hofseth <karl@kehofseth.no>
2022-10-05 16:37:51 +0200

    Fix config for all urls (#65)
    * Fix config for all urls
    
    * Collect static files in docker image

--- sleepynativeapp/app.config.js
+++ sleepynativeapp/app.config.js
@@ -7,6 +7,7 @@ export default (parent = {}) => {
     // environment
     KRATOS_URL = "https://somnus.idi.ntnu.no/auth",
     STATIC_URL = "https://somnus.idi.ntnu.no/static",
+    API_URL = "https://somnus.idi.ntnu.no/api",
   } = env;
 
   return {
@@ -14,6 +15,10 @@ export default (parent = {}) => {
     extra: {
       kratosUrl: KRATOS_URL,
       staticUrl: STATIC_URL,
+      apiUrl: API_URL,
+      eas: {
+        projectId: "4682f5a6-523f-4b8b-b5d2-08754e05b2c0",
+      },
     },
   };
 };
