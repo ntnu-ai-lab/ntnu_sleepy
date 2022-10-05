417bfda19ca3b95a064f5206c39382eaa5059c77
Karl Erik Hofseth <karl@kehofseth.no>
2022-10-05 12:25:55 +0200

    Configure stuff on the vm, and configure release channels for pull requests (#63)
    * Change allowed hosts
    
    * Update backend urls
    
    * Publish expo builds for pull requests as well
    
    * Rename pipelines

--- sleepynativeapp/app.config.js
+++ sleepynativeapp/app.config.js
@@ -1,19 +1,19 @@
-
 export default (parent = {}) => {
-    // We gracefully destruct these parameters to avoid "undefined" errors:
-    const { config = {} } = parent
-    const { env = {} } = process || {}
-  
-    const {
-      // environment
-      KRATOS_URL = "http://10.0.2.2:4433",
+  // We gracefully destruct these parameters to avoid "undefined" errors:
+  const { config = {} } = parent;
+  const { env = {} } = process || {};
 
-    } = env
-  
-    return {
-      ...config,
-      extra: {
-        kratosUrl: KRATOS_URL,
-      },
-    }
-  }
+  const {
+    // environment
+    KRATOS_URL = "https://somnus.idi.ntnu.no/auth",
+    STATIC_URL = "https://somnus.idi.ntnu.no/static",
+  } = env;
+
+  return {
+    ...config,
+    extra: {
+      kratosUrl: KRATOS_URL,
+      staticUrl: STATIC_URL,
+    },
+  };
+};
