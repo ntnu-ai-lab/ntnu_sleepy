e8373f2c7edd4af036cc1916fced4109330e3b59
hildmi@stud.ntnu.no <hildmi@stud.ntnu.no>
2022-10-18 09:28:14 +0200

    matomo tracker and instance

--- sleepynativeapp/App.tsx
+++ sleepynativeapp/App.tsx
@@ -1,24 +1,48 @@
 import "react-native-url-polyfill/auto";
 import ProjectProvider from "./auth/ProjectProvider";
-import React from "react";
+import React, { useEffect } from "react";
 import AuthProvider from "./auth/AuthProvider";
 import { RecoilRoot } from "recoil";
 import { StorageController } from "./devicestorage/StorageController";
 import { Provider as PaperProvider } from "react-native-paper";
 import { Navigation } from "./components/Navigation";
+import MatomoTracker, {
+  MatomoProvider,
+  useMatomo,
+} from "matomo-tracker-react-native";
+import { View } from "react-native";
+
+const MainAppContainer = () => {
+  const { trackAppStart } = useMatomo();
+
+  useEffect(() => {
+    trackAppStart;
+  }, []);
+
+  return <View>Main App</View>;
+};
 
 export default function App() {
+  const instance = new MatomoTracker({
+    urlBase: "https://matomo.idi.ntnu.no/",
+    siteId: 9,
+  });
   return (
-    <ProjectProvider>
-      <AuthProvider>
-        <PaperProvider>
-          <RecoilRoot>
-            <StorageController>
-              <Navigation />
-            </StorageController>
-          </RecoilRoot>
-        </PaperProvider>
-      </AuthProvider>
-    </ProjectProvider>
+    <>
+      <MatomoProvider instance={instance}>
+        <MainAppContainer />
+      </MatomoProvider>
+      <ProjectProvider>
+        <AuthProvider>
+          <PaperProvider>
+            <RecoilRoot>
+              <StorageController>
+                <Navigation />
+              </StorageController>
+            </RecoilRoot>
+          </PaperProvider>
+        </AuthProvider>
+      </ProjectProvider>
+    </>
   );
 }
