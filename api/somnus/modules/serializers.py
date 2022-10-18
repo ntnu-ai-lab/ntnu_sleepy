1a87f114a9575b47fc9a888692d5a90f6b532e98
Karl Erik Hofseth <karl@kehofseth.no>
2022-10-18 13:07:15 +0200

    Add serialization for rules and rule groups

--- api/somnus/modules/serializers.py
+++ api/somnus/modules/serializers.py
@@ -2,43 +2,98 @@ from typing import Any
 from rest_framework import serializers
 from rest_flex_fields import FlexFieldsModelSerializer
 
-from .models import Answer, AnswerList, FormSection, ImageSection, Input, Module, Page, Part, Section, TextSection, VideoSection
+from .models import Answer, AnswerList, FormSection, ImageSection, Input, Module, Page, Part, Rule, RuleGroup, Section, TextSection, VideoSection
+
+class RuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rule
+        fields = ['id', 'input', 'comparator', 'value', 'should_be']
+
+    def to_representation(self, instance: Rule) -> Any:
+        try:
+            instance.evaluate(self.context['request'].user)
+            return None
+        except Answer.DoesNotExist:
+            return super().to_representation(instance)
+
+
+class RuleGroupSerializer(serializers.ModelSerializer):
+    rules = serializers.SerializerMethodField('get_rule_serializer')
+    class Meta:
+        model = RuleGroup
+        fields = ['id', 'rules']
+        depth = 1
+
+    def to_representation(self, instance: RuleGroup) -> Any:
+        try:
+            instance.evaluate(self.context['request'].user, catch=False)
+            return None
+        except Answer.DoesNotExist:
+            return super().to_representation(instance)
+
+
+    def get_rule_serializer(self, obj: RuleGroup) -> Any:
+        serializer_context = {'request': self.context.get('request') }
+        children = Rule.objects.all().filter(rule_group=obj)
+        serializer = RuleSerializer(children, many=True, context=serializer_context)
+        return serializer.data
 
 class InputSerializer(serializers.ModelSerializer[Input]):
+    rules = serializers.SerializerMethodField('get_rule_group_serializer')
     class Meta:
         model = Input
-        fields = ['type', 'name', 'label', 'helptext', 'value', 'answers', 'section']
+        fields = ['type', 'name', 'label', 'helptext', 'value', 'answers', 'rules']
 
     def to_representation(self, instance: Input) -> Any:
         return super().to_representation(instance) if instance.evaluate_rules(self.context['request'].user) else None
 
+    def get_rule_group_serializer(self, obj: Input) -> Any:
+        serializer_context = {'request': self.context.get('request') }
+        children = RuleGroup.objects.all().filter(input=obj)
+        serializer = RuleGroupSerializer(children, many=True, context=serializer_context)
+        return serializer.data
+
 class SectionSerializer(serializers.ModelSerializer[Section]):
+    rules = serializers.SerializerMethodField('get_rule_group_serializer')
     class Meta:
         model = Section
-        fields = ['id', 'heading', 'content', 'page', 'type']
+        fields = ['id', 'heading', 'content', 'type', 'rules']
+    
+    def get_rule_group_serializer(self, obj: Section) -> Any:
+        serializer_context = {'request': self.context.get('request') }
+        children = RuleGroup.objects.all().filter(section=obj)
+        serializer = RuleGroupSerializer(children, many=True, context=serializer_context)
+        return serializer.data
 
 
-class TextSectionSerializer(serializers.ModelSerializer[TextSection]):
+class TextSectionSerializer(SectionSerializer):
     class Meta(SectionSerializer.Meta):
         model = TextSection
 
-class ImageSectionSerializer(serializers.ModelSerializer[ImageSection]):
+class ImageSectionSerializer(SectionSerializer):
     class Meta(SectionSerializer.Meta):
         model = ImageSection
         fields = SectionSerializer.Meta.fields + ['uri']
 
-class VideoSectionSerializer(serializers.ModelSerializer[VideoSection]):
+class VideoSectionSerializer(SectionSerializer):
     class Meta(SectionSerializer.Meta):
         model = VideoSection
         fields = SectionSerializer.Meta.fields + ['uri']
 
 class FormSectionSerializer(FlexFieldsModelSerializer[FormSection]): # type: ignore[no-any-unimported]
+    rules = serializers.SerializerMethodField('get_rule_group_serializer')
     expandable_fields = {
         'form': (InputSerializer) 
     }
     class Meta(SectionSerializer.Meta):
         model = FormSection
         fields = SectionSerializer.Meta.fields + ['form']
+
+    def get_rule_group_serializer(self, obj: FormSection) -> Any:
+        serializer_context = {'request': self.context.get('request') }
+        children = RuleGroup.objects.all().filter(section=obj)
+        serializer = RuleGroupSerializer(children, many=True, context=serializer_context)
+        return serializer.data
 
 class ChildSectionSerializer(serializers.Serializer):
     serializers = {
@@ -49,7 +104,7 @@ class ChildSectionSerializer(serializers.Serializer):
     }
     def to_representation(self, instance: Section) -> Any:
         section = Section.objects.get_subclass(id=instance.id) if type(instance) == Section else instance
-        return self.serializers[type(section)](section).data if section.evaluate_rules(self.context['request'].user) else None
+        return self.serializers[type(section)](section, context=self.context).data if section.evaluate_rules(self.context['request'].user) else None
 
 class PageSerializer(FlexFieldsModelSerializer[Page]): # type: ignore [no-any-unimported]
     class Meta:
