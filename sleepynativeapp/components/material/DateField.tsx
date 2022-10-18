b479fe806a35b0acc7e8a14c31bde48b10365217
Karl Erik Hofseth <karl@kehofseth.no>
2022-10-18 14:51:29 +0200

    Add a DateField component

---
+++ sleepynativeapp/components/material/DateField.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { TextField } from "./TextField";
+
+const timeRegex: RegExp = /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+
+interface DateFieldState {
+  string: string;
+  date?: Date;
+  correct: boolean;
+}
+
+export function DateField({
+  initialState = { string: "", correct: true },
+  onChange,
+}: {
+  initialState: DateFieldState;
+  onChange: (date: Date) => void;
+}) {
+  const [state, setState] = useState<DateFieldState>(initialState);
+
+  const [date] = useState(new Date()); // We don't want this to change every time the component renders.
+
+  return (
+    <TextField
+      style={{ minWidth: 150, maxWidth: "30%", alignItems: "center" }}
+      error={!state.correct}
+      placeholderText={"HH:MM"}
+      onChange={(e) => {
+        setState((time) => {
+          time.string = e;
+          if (e.length < 4 || (e.length < 5 && e.includes(":"))) {
+            time.correct = true;
+          } else {
+            time.correct = timeRegex.test(e);
+          }
+          if (timeRegex.test(e)) {
+            time.date = new Date(
+              date.getFullYear(),
+              date.getMonth(),
+              date.getDate(),
+              parseInt(e.slice(0, 2)),
+              parseInt(e.slice(3))
+            );
+            onChange(time.date);
+          }
+          return { ...time };
+        });
+      }}
+      value={state.string}
+    />
+  );
+}
