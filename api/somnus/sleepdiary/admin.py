44eab37266148237c94c9ccddd816bf269322fc9
Karl Erik Hofseth <karl@kehofseth.no>
2022-10-20 15:41:35 +0200

    Add diary entries inline

--- api/somnus/sleepdiary/admin.py
+++ api/somnus/sleepdiary/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import SleepDiary
+from .models import DiaryEntry, SleepDiary
 
+class DiaryEntryStackedInline(admin.StackedInline):
+    extra = 0
+    model = DiaryEntry
+    exclude = ('id',)
 class SleepDiaryAdmin(admin.ModelAdmin):
+    inlines = [DiaryEntryStackedInline]
     exclude = ('id',)
 
 admin.site.register(SleepDiary, SleepDiaryAdmin)
