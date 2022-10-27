f6c7cb8b89c3d29e2cfdc6fb0c9c56f89fed2d99
hildmi@stud.ntnu.no <hildmi@stud.ntnu.no>
2022-10-27 11:10:26 +0200

    email and date validation

--- sleepynativeapp/components/material/TimeField.tsx
+++ sleepynativeapp/components/material/TimeField.tsx
@@ -1,44 +1,53 @@
 import React, { useState } from "react";
 import { TextField } from "./TextField";
 
-const timeRegex = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
+const timeRegex: RegExp = /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
 
-interface DateFieldState {
+interface TimeFieldState {
   string: string;
   date?: Date;
   valid: boolean;
 }
 
-export function DateField({
+export function TimeField({
   initialState = { string: "", valid: true },
   onChange,
+  baseDate = new Date(),
   style,
 }: {
-  initialState?: DateFieldState;
+  initialState?: TimeFieldState;
   onChange: (date: Date | false) => void;
   baseDate?: Date;
   style?: any;
 }) {
-  const [state, setState] = useState<DateFieldState>(initialState);
-  const [hasLostFocus, setHasLostFocus] = useState(false);
+  const [state, setState] = useState<TimeFieldState>(initialState);
+  const [hasLostFocus, setHastLostFocus] = useState(false);
 
   return (
     <TextField
-      onEndEditing={() => setHasLostFocus(true)}
-      style={{ ...style, minWidth: "40%", alignItems: "center" }}
+      onEndEditing={() => setHastLostFocus(true)}
+      style={{
+        ...style,
+        minWidth: "30%",
+        maxWidth: "60%",
+        marginHorizontal: "5%",
+        alignItems: "center",
+        alignSelf: "center",
+      }}
       error={hasLostFocus && !state.valid}
-      placeholderText={"YYYY-MM-DD"}
+      placeholderText={"HH:MM"}
       onChange={(e) => {
         setState((time) => {
           time.string = e;
           time.valid = timeRegex.test(e);
-          console.log(e);
           console.log(time.valid);
           if (time.valid) {
             time.date = new Date(
-              parseInt(e.slice(0, 4)),
-              parseInt(e.slice(5, 7)) - 1,
-              parseInt(e.slice(8, 10))
+              baseDate.getFullYear(),
+              baseDate.getMonth(),
+              baseDate.getDate(),
+              parseInt(e.slice(0, 2)),
+              parseInt(e.slice(3))
             );
             onChange(time.date);
           } else {
