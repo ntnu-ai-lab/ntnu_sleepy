23db298ad98a8ad03fe3f861b3a977d32c433d60
Magnus Lykke Dahlø <magnusld@stud.ntnu.no>
2022-11-03 12:54:41 +0100

    feat(#86): Legge til innsending av svar fra forms (#155)
    * feat(#86): Legge til innsending av svar fra forms
    
    * chore(): Remove console.log

--- sleepynativeapp/api/formApi.ts
+++ sleepynativeapp/api/formApi.ts
@@ -2,7 +2,7 @@ import { callApi } from "./callApi";
 import { AnswerList } from "../types/modules";
 
 export async function sendAnswerList(answerlist: AnswerList) {
-    await callApi<never>(`modules/answer-list/`, {
+    await callApi<never>(`modules/answer-lists/`, {
         method: "POST",
         body: JSON.stringify(answerlist)
     });
