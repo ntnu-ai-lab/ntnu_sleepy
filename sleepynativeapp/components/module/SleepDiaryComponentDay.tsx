1b7235d70382934250907686369a83e5eff8c488
simonevo <simonevo@stud.ntnu.no>
2022-11-10 13:48:05 +0100

    Fix datefield in sleepdiarycomponentday

--- sleepynativeapp/components/module/SleepDiaryComponentDay.tsx
+++ sleepynativeapp/components/module/SleepDiaryComponentDay.tsx
@@ -170,7 +170,7 @@ export default function SleepDiaryComponentDay() {
               ? date.getMonth() + 1
               : "0" + date.getMonth() + 1) +
             "-" +
-            (date.getDate() > 10 ? date.getDate() : "0" + date.getDate()),
+            (date.getDate() >= 10 ? date.getDate() : "0" + date.getDate()),
           date: date,
           valid: true,
         }}
