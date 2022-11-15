212dec478fdc1d0d9accfdca6883521ea8233f9c
Karl Erik Hofseth <karl@kehofseth.no>
2022-11-15 10:25:22 +0100

    Use Profile object

--- sleepynativeapp/api/userApi.ts
+++ sleepynativeapp/api/userApi.ts
@@ -1,28 +1,38 @@
 import { storeLocalUser } from "../state/StorageController";
 import { callApi } from "./callApi";
-import { User } from "../types/Types";
+import { User, Profile, DjangoUser } from "../types/Types";
 
-export async function createUser(user: User, identiyId: string) {
-  const response = await callApi<User>(`users/${identiyId}/`, {
+export async function createUser(user: DjangoUser, identiyId: string) {
+  const response = await callApi<DjangoUser>(`users/${identiyId}/`, {
     method: "PATCH",
     body: JSON.stringify(user),
   });
-  return response.data
+  return response.data;
 }
 
-export async function readyUserForSleepRestriction(user: User, identiyId: string) {
+export async function updateProfile(profile: Profile, identityId: string) {
+  const response = await callApi<Profile>(`users/profiles/${identityId}/`, {
+    method: "PATCH",
+    body: JSON.stringify(profile),
+  });
+  return response.data;
+}
 
+export async function readyUserForSleepRestriction(
+  user: User,
+  identiyId: string
+) {
   user.sleepRestriction = true;
 
   const response = await callApi<User>(`users/${identiyId}/`, {
     method: "PATCH",
     body: JSON.stringify(user),
   });
-  return response.data
+  return response.data;
 }
 
 export async function getUserByIdentiyId(identiyId: string) {
-  const response = await callApi<User>(`users/${identiyId}/`, {
+  const response = await callApi<User>(`users/profiles/${identiyId}/`, {
     method: "GET",
   });
   if (!response.response.ok) return undefined;
@@ -36,6 +46,6 @@ export async function getUserByIdentiyId(identiyId: string) {
       relationshipStatus: response.data.relationshipStatus,
     };
     storeLocalUser(user);
-    return user
+    return user;
   }
 }
