fcce0c30815acb19b8280abf270c49c2ab9fe1bd
Karl Erik Hofseth <karl@kehofseth.no>
2022-11-17 13:39:03 +0100

    Add admin form for Profile

--- api/somnus/users/admin.py
+++ api/somnus/users/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Profile
 
 class UserAdmin(admin.ModelAdmin):
     class Meta:
         exclude = ('id',)
 
+class ProfileAdmin(admin.ModelAdmin):
+    class Meta:
+        exclude = ('id',)
+
 admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
