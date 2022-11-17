e81622add6015f357da05cf30d3737a7e22cc31d
Karl Erik Hofseth <karl@kehofseth.no>
2022-11-17 16:29:11 +0100

    Add admin panel for sleep restriction plans

--- api/somnus/sleepdiary/admin.py
+++ api/somnus/sleepdiary/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import DiaryEntry, SleepDiary
+from .models import DiaryEntry, SleepDiary, SleepRestrictionPlan
 
 class DiaryEntryStackedInline(admin.StackedInline):
     extra = 0
@@ -10,4 +10,8 @@ class SleepDiaryAdmin(admin.ModelAdmin):
     inlines = [DiaryEntryStackedInline]
     exclude = ('id',)
 
+class SleepRestrictionAdmin(admin.ModelAdmin):
+    exclude = ('id',)
+
 admin.site.register(SleepDiary, SleepDiaryAdmin)
+admin.site.register(SleepRestrictionPlan, SleepRestrictionAdmin)
