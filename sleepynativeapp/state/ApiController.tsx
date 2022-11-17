6ebf5533793952c074a2905d56d991314f7eb953
Karl Erik Hofseth <karl@kehofseth.no>
2022-11-17 16:33:26 +0100

    Get sleep diary when logging in

--- sleepynativeapp/state/ApiController.tsx
+++ sleepynativeapp/state/ApiController.tsx
@@ -1,21 +1,30 @@
 import { ReactNode, useContext, useEffect, useState } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { getAllModules } from "../api/modulesApi";
+import { getDiary, listDiaryEntries } from "../api/sleepDiaryApi";
 import { AuthContext } from "../auth/AuthProvider";
-import { loggedInUser, moduleIds } from "./atoms";
+import { loggedInUser, moduleIds, cachedSleepDiary } from "./atoms";
 
 export function ApiController(props: { children: ReactNode | ReactNode[] }) {
   const { children } = props;
   const { isAuthenticated } = useContext(AuthContext);
-  const user = useRecoilValue(loggedInUser)
-  const [mIds, setMIds] = useRecoilState(moduleIds)
+  const user = useRecoilValue(loggedInUser);
+  const [mIds, setMIds] = useRecoilState(moduleIds);
+  const [, setCachedSleepDiary] = useRecoilState(cachedSleepDiary);
 
   useEffect(() => {
     if (isAuthenticated && user) {
       getAllModules().then((r) => {
-        setMIds(r)});
+        setMIds(r);
+      });
+      getDiary().then((d) => {
+        d &&
+          listDiaryEntries(d.id).then(
+            (e) => e && setCachedSleepDiary({ ...d, diary_entries: e })
+          );
+      });
     }
-  }, [isAuthenticated]);
+  }, [isAuthenticated, user]);
 
   return <>{children}</>;
 }
