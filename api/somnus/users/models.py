e1f8e11ff63a4e1bbfd846b7597f0953730fbb9b
Karl Erik Hofseth <karl.e.hofseth@infomedia.dk>
2022-11-20 17:32:34 +0100

    Fix createsuperuser command

--- api/somnus/users/models.py
+++ api/somnus/users/models.py
@@ -3,10 +3,12 @@ from typing import TYPE_CHECKING, Any
 
 import uuid
 from django.db import models
+from django.apps import apps
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.contrib.auth.base_user import BaseUserManager as BaseUserManager
 from django.contrib.auth.models import UserManager
+from django.contrib.auth.hashers import make_password
 from django.utils.translation import gettext_lazy as _
 
 
@@ -35,6 +37,26 @@ class Relationshipstatus(models.TextChoices):
     email = models.EmailField()
     answers: models.Manager['Answer'] """
 
+
+class SomnusUserManager(UserManager):
+    """Override of UserManager just to remove email from _create_user since we don't have an email field."""
+    def _create_user(self, username: str, _: None, password: str, **extra_fields: Any) -> 'User':
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not username:
+            raise ValueError("The given username must be set")
+        # Lookup the real model class from the global app registry so this
+        # manager method can be used in migrations. This is fine because
+        # managers are by definition working on the real model.
+        GlobalUserModel = apps.get_model(
+            self.model._meta.app_label, self.model._meta.object_name
+        )
+        username = GlobalUserModel.normalize_username(username)
+        user: User = self.model(username=username, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
 
 class User(AbstractBaseUser, PermissionsMixin):
     """
@@ -74,15 +96,13 @@ class User(AbstractBaseUser, PermissionsMixin):
     date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
     is_superuser = models.BooleanField(default=False)
 
-    objects = UserManager()
+    objects = SomnusUserManager()
 
     answers: models.Manager['Answer']
     diary: models.Manager['SleepDiary']
     sleep_restriction_plan: models.Manager['SleepRestrictionPlan']
 
-    EMAIL_FIELD = "email"
     USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ["email"]
 
     @property
     def details(self) -> 'Profile | None':
